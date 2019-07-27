e211014e90c53fea884879c4e43fea0806a755bd
Buvaneshwari-m <52272256+Buvaneshwari-m@users.noreply.github.com>
2019-07-27 13:51:42 +0530

    Create pro14.py

---
+++ pro14.py
@@ -0,0 +1,13 @@
+dal,hotel=map(int,input().split())
+list1=list(map(int,input().split()))
+dal=[]
+list1.insert(0,0)
+for y in range(hotel):
+     vin=[]
+     ji=0
+     zz,oo=map(int,input().split())
+     for i in range(zz,oo+1):         
+         ji^=list1[i]     
+     dal.append(s)
+for y in dal:
+     print(y)
