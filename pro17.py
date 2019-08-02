7bdc8797bed23215702a473069ec9ca3561a45e0
Buvaneshwari-m <52272256+Buvaneshwari-m@users.noreply.github.com>
2019-08-02 14:54:09 +0530

    Update pro17.py

--- pro17.py
+++ pro17.py
@@ -1,9 +1,14 @@
-from itertools import troll
-born, guf = map(int, input().split())
-x = list(map(int, input().split()))
-for hlo in troll(x, 2):
-    if sum(hlo) == guf:
-        print('yes')
-        break
-else:
-    print('no')
+ro_17=list(map(int,input().split()))
+uut1=ro_17[1]
+flag1=0
+uu_17=list(map(int,input().split()))
+for i in range(0,len(uu_17)-1):
+	for j in range(i+1,len(uu_17)):
+		if uu_17[i]+uu_17[j]==uut1:
+			print("yes")
+			flag1=1
+			break
+	if flag1==1:
+		break
+if flag1!=1:
+	print("no")
