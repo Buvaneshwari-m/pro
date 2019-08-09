9f0c328d477b67e86f961fec152821284572f194
Buvaneshwari-m <52272256+Buvaneshwari-m@users.noreply.github.com>
2019-08-09 13:39:01 +0530

    Update 34pro.py

--- 34pro.py
+++ 34pro.py
@@ -1,7 +1,12 @@
-kora1 = int(input())
-juice = []
-for i in range(pow(2, kora1)):
-    juice.append(bin(i)[2:].zfill(kora1))
-juice.sort(key=(lambda x: x.count('1')))
-for i in juice:
-    print(i) 
+gira,push=map(int,input().split())
+jos1=0
+lithi1=[]
+for i in range(gira):
+      Lithi1.append(input())
+for i in range(gira):
+      for j in range(push-1):
+            if(Lithi1[i][j]!='R' and Lithi1[i][j+1]!='R'):
+                  jos1+=3
+            elif(Lithi1[i][j]!='G' and Lithi1[i][j+1]!='G'):
+                  jos1+=5
+print(jos11)
